e52c97229b778784b3cf67e75fb9d04aeeb9163b
fred.feng <fred.feng@thomascook.com.cn>
2021-01-13 15:21:32 +0800

    feat: add eslint prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
 
 // const { readdirSync } = require('fs')
 // const getInput = () => {
@@ -18,47 +18,45 @@ import resolve from 'rollup-plugin-node-resolve'
 // }
 
 export default {
-  input: 'src/index.tsx',
+  input: "src/index.tsx",
   output: [
     {
-      dir: 'lib',
-      format: 'cjs',
-      exports: 'named',
+      dir: "lib",
+      format: "cjs",
+      exports: "named",
       sourcemap: true,
       preserveModules: true,
-      preserveModulesRoot: 'src'
+      preserveModulesRoot: "src",
     },
     {
-      dir: 'es',
-      format: 'es',
-      exports: 'named',
+      dir: "es",
+      format: "es",
+      exports: "named",
       sourcemap: true,
       preserveModules: true,
-      preserveModulesRoot: 'src'
-    }
+      preserveModulesRoot: "src",
+    },
   ],
-  external: ['antd'],
+  external: ["antd"],
   plugins: [
     external(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
-      exclude: [
-        '**/__tests__/**'
-      ],
-      clean: true
+      exclude: ["**/__tests__/**"],
+      clean: true,
     }),
     commonjs({
-      include: ['node_modules/**'],
+      include: ["node_modules/**"],
       namedExports: {
-        'node_modules/react/react.js': [
-          'Children',
-          'Component',
-          'PropTypes',
-          'createElement'
+        "node_modules/react/react.js": [
+          "Children",
+          "Component",
+          "PropTypes",
+          "createElement",
         ],
-        'node_modules/react-dom/index.js': ['render']
-      }
-    })
-  ]
-}
+        "node_modules/react-dom/index.js": ["render"],
+      },
+    }),
+  ],
+};
