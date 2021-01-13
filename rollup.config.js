720e2d82cb1b2138b33341b227c1293ee50130ab
fred.feng <fred.feng@thomascook.com.cn>
2021-01-14 00:30:21 +0800

    rollup scss

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,8 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
-
-// const { readdirSync } = require('fs')
-// const getInput = () => {
-//   const input = { index: 'src/index.tsx' }
-
-//   const files = readdirSync('src', { encoding: 'utf8', withFileTypes: false })
-//   files.forEach((item) => {
-//     if (item !== 'index.tsx') {
-//       input[item] = `src/${item}/index.tsx`
-//     }
-//   })
-
-//   return input
-// }
+import postcss from 'rollup-plugin-postcss'
 
 export default {
   input: 'src/index.tsx',
@@ -38,25 +25,5 @@ export default {
     },
   ],
   external: ['antd'],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      exclude: ['**/__tests__/**'],
-      clean: true,
-    }),
-    commonjs({
-      include: ['node_modules/**'],
-      namedExports: {
-        'node_modules/react/react.js': [
-          'Children',
-          'Component',
-          'PropTypes',
-          'createElement',
-        ],
-        'node_modules/react-dom/index.js': ['render'],
-      },
-    }),
-  ],
+  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), postcss()],
 }
--- src/Counter/__tests__/Counter.spec.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { render } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Button from '..'
-
-test('Renders', async () => {
-  const { getByRole } = render(<Button text='ss' />)
-  expect(getByRole('heading')).toHaveTextContent('My First Component')
-})
