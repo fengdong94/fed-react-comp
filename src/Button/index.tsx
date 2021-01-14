986dba7fc425e9738b4ca3901c06937b7a02ec9b
fred.feng <fred.feng@thomascook.com.cn>
2021-01-14 10:30:11 +0800

    add jest

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './index.scss'
 
-interface Props {
+export interface Props {
   type: 'primary' | 'secondary' | 'third'
   children: unknown
 }
 
 const Button: React.FC<Props> = ({ type, children }) => {
-  return <button className={`btn ${type}`}>{children}</button>
+  return (
+    <button data-testid='test-component-button' className={`btn ${type}`}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
